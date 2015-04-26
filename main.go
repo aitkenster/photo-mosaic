72e8435a936e90936024dc49050fd07b6bc54a5a
Nicola Aitken <nicola@geckoboard.com>
2015-04-26 12:59:44 +0100

    upload files to tmp dir

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"io/ioutil"
 	"fmt"
+	"os"
+	"image/png"
+	"image/jpeg"
 )
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,7 +14,44 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, string(body))
 }
 
+func viewFileHandler(w http.ResponseWriter, r *http.Request) {
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		fmt.Fprint(w, "Error @ 1")
+		fmt.Fprint(w, err)
+		return
+	}
+
+	img, err := png.Decode(file)
+	if err != nil {
+		fmt.Fprint(w, "Error @ 2")
+		fmt.Fprint(w, err)
+		return
+	}
+
+	out, err := os.Create("./tmp/uploadedfile.jpeg")
+	if err != nil {
+		fmt.Fprint(w, "Error @ 3")
+		fmt.Fprint(w, err)
+
+	}
+	defer out.Close()
+
+	err = jpeg.Encode(out, img, nil)
+	if err != nil {
+		fmt.Fprint(w, "Error @ 4")
+		fmt.Fprint(w, err)
+		return
+	}
+
+
+
+	fmt.Fprint(w, "Image uploaded successfully")
+}
+
 func main () {
 	http.HandleFunc("/", uploadHandler)
+	http.HandleFunc("/view", viewFileHandler)
 	http.ListenAndServe(":8080", nil)
+
 }
