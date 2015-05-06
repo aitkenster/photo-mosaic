72d3ecbc39188ddf3216ff9a69067fa57800e495
Nicola Aitken <nicola@geckoboard.com>
2015-05-06 17:26:02 +0100

    finds colour of pixel group

--- edit_image.go
+++ edit_image.go
@@ -28,9 +28,9 @@ func main (){
 //crop the main image
 	crop_rect := image.NewRGBA(image.Rect(0, 0, 300, 150))
 	draw.Draw(crop_rect, crop_rect.Bounds(), img, image.Point{0,0}, draw.Src)
-
-	mini_image := getMiniImage("test_image2.jpeg")
-	addMiniImages(crop_rect, mini_image)
+	getImageAverageColors(crop_rect)
+	//mini_image := getMiniImage("test_image2.jpeg")
+	//addMiniImages(crop_rect, mini_image)
 	out, err := os.Create("altered_test_image.jpeg")
 	if err != nil {
 		fmt.Print("Error @ img3")
@@ -50,53 +50,74 @@ func main (){
 
 }
 
-func addMiniImages(main_image *image.RGBA, mini_image *image.RGBA) {
-	var start_point image.Point
+//func addMiniImages(main_image *image.RGBA, mini_image *image.RGBA) {
+	//var start_point image.Point
+	//size := main_image.Bounds().Size()
+	//for x := 0; x < size.X; x += 20 {
+		//for y := 0; y < size.Y; y += 10 {
+			//start_point = image.Pt(x, y)
+			//min_point := image.Pt(0,0)
+			//r := image.Rectangle{start_point, start_point.Add(mini_image.Bounds().Size())}
+			//draw.Draw(main_image, r, mini_image, min_point, draw.Src)
+		//}
+	//}
+//}
+
+//takes each 10x10 pixel block and returns the average color
+func getImageAverageColors(main_image *image.RGBA) {
 	size := main_image.Bounds().Size()
-	for x := 0; x < size.X; x += 20 {
+	for x := 0; x < size.X; x += 10 {
 		for y := 0; y < size.Y; y += 10 {
-			start_point = image.Pt(x, y)
-			min_point := image.Pt(0,0)
-			r := image.Rectangle{start_point, start_point.Add(mini_image.Bounds().Size())}
-			draw.Draw(main_image, r, mini_image, min_point, draw.Src)
+			start_point := image.Pt(x, y)
+			end_point := image.Pt(x+10, y+10)
+			fmt.Println(start_point)
+			r := image.Rectangle{start_point, end_point}
+			m := main_image.SubImage(r)
+			average_color := averageColor(m)
+			fmt.Println(average_color)
+			draw.Draw(main_image, r, &image.Uniform{average_color}, image.ZP, draw.Src)
 		}
 	}
 }
 
-func getMiniImage(filename string) (*image.RGBA) {
-	mini_file, err := os.Open(filename)
-	if err != nil {
-		fmt.Print("Error @ img5")
-		fmt.Println(err)
-	}
-	defer mini_file.Close()
+//func getMiniImage(filename string) (*image.RGBA) {
+	//mini_file, err := os.Open(filename)
+	//if err != nil {
+		//fmt.Print("Error @ img5")
+		//fmt.Println(err)
+	//}
+	//defer mini_file.Close()
 
-	mini_image, _, err  := image.Decode(mini_file)
-	if err != nil {
-		fmt.Print("Error @ img6")
-		fmt.Println(err)
-	}
+	//mini_image, _, err  := image.Decode(mini_file)
+	//if err != nil {
+		//fmt.Print("Error @ img6")
+		//fmt.Println(err)
+	//}
 
-//crop the mini image(this needs to be resize or something really)
-	cropped_mini := image.NewRGBA(image.Rect(0, 0, 10, 10))
-	draw.Draw(cropped_mini, cropped_mini.Bounds(), mini_image, image.Point{0,0}, draw.Src)
-	analyseColors(cropped_mini)
-	return cropped_mini
-}
+////crop the mini image(this needs to be resize or something really)
+	//cropped_mini := image.NewRGBA(image.Rect(0, 0, 10, 10))
+	//draw.Draw(cropped_mini, cropped_mini.Bounds(), mini_image, image.Point{0,0}, draw.Src)
+	//analyseColors(cropped_mini)
+	//return cropped_mini
+//}
 
-func analyseColors(image *image.RGBA) {
-	size := image.Bounds().Size()
-	r := 0
-	g := 0
-	b := 0
-	number_pixels := 0
-	for x := 0; x < size.X; x ++ {
-		for y := 0; y < size.Y; y++ {
-			color := image.RGBAAt(x,y)
-			fmt.Println(color.R)
-			r += int(color.R)
-			g += int(color.G)
-			b += int(color.B)
+func averageColor(img image.Image) (color.RGBA) {
+	bounds := img.Bounds()
+
+	minX := bounds.Min.X
+	maxX := bounds.Max.X
+
+	minY := bounds.Min.Y
+	maxY := bounds.Max.Y
+
+	var r, g, b, number_pixels int
+	for x := minX; x < maxX; x ++ {
+		for y := minY; y < maxY; y++ {
+			pixel := img.At(x,y)
+			rgbaColor := color.RGBAModel.Convert(pixel).(color.RGBA)
+			r += int(rgbaColor.R)
+			g += int(rgbaColor.G)
+			b += int(rgbaColor.B)
 			number_pixels ++
 		}
 	}
@@ -104,8 +125,9 @@ func analyseColors(image *image.RGBA) {
 		R: uint8(r/number_pixels),
 		G: uint8(g/number_pixels),
 		B: uint8(b/number_pixels),
-		A: uint8(1),
+		A: 1,
 	}
+	fmt.Println(averageColor)
 	return averageColor
 
 }
