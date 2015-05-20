fd707961fb9753f5898dfcbe4233ab1e18ddabed
Nicola Aitken <nicola@geckoboard.com>
2015-05-20 13:45:12 +0100

    creates a rough canvas mosaic

--- edit_image.go
+++ edit_image.go
@@ -5,6 +5,7 @@ import (
 	"image/jpeg"
 	"image/color"
 	"image/draw"
+	"github.com/disintegration/imaging"
 	"os"
 	"path/filepath"
 	"fmt"
@@ -32,42 +33,30 @@ func main (){
 	image_averages := getImageAverageColors(crop_rect)
 	//mini_image := getMiniImage("test_image2.jpeg")
 	//addMiniImages(crop_rect, mini_image)
-	out, err := os.Create("altered_test_image.jpeg")
+	mosaic, err := os.Create("altered_test_image.jpeg")
 	if err != nil {
 		fmt.Print("Error @ img3")
 		fmt.Println(err)
 		return
 	}
 
-	defer out.Close()
+	defer mosaic.Close()
 
-	err = jpeg.Encode(out, crop_rect, nil)
-	if err != nil {
-		fmt.Print("Error @ img4")
-		fmt.Println(err)
-		return
-	}
 	image_color_dictionary := processMosaicTiles()
 	tile_positions := make(map[image.Point]string)
 	for point, color := range image_averages {
 		tile_positions[point] = findClosestColorMatch(color, image_color_dictionary)
 	}
-	fmt.Println(tile_positions)
+	createTileCanvas(tile_positions, crop_rect)
 
+	err = jpeg.Encode(mosaic, crop_rect, nil)
+	if err != nil {
+		fmt.Print("Error @ img4")
+		fmt.Println(err)
+		return
+	}
 }
 
-//func addMiniImages(main_image *image.RGBA, mini_image *image.RGBA) {
-	//var start_point image.Point
-	//size := main_image.Bounds().Size()
-	//for x := 0; x < size.X; x += 20 {
-		//for y := 0; y < size.Y; y += 10 {
-			//start_point = image.Pt(x, y)
-			//min_point := image.Pt(0,0)
-			//r := image.Rectangle{start_point, start_point.Add(mini_image.Bounds().Size())}
-			//draw.Draw(main_image, r, mini_image, min_point, draw.Src)
-		//}
-	//}
-//}
 
 //takes each 10x10 pixel block and returns the average color
 func getImageAverageColors(main_image *image.RGBA) map[image.Point]color.RGBA{
@@ -116,10 +105,8 @@ func processMosaicTiles()(map[color.RGBA] string) {
 
 
 	if err != nil {
-		fmt.Println("yo")
 		fmt.Println(err)
 	}
-	fmt.Println(image_color_dictionary)
 	return image_color_dictionary
 }
 
@@ -132,27 +119,6 @@ func findClosestColorMatch(average_color color.RGBA, image_color_dictionary map[
 
 	return image_color_dictionary[color.RGBAModel.Convert(closet_color_match).(color.RGBA)]
 }
-
-//func getMiniImage(filename string) (*image.RGBA) {
-	//mini_file, err := os.Open(filename)
-	//if err != nil {
-		//fmt.Print("Error @ img5")
-		//fmt.Println(err)
-	//}
-	//defer mini_file.Close()
-
-	//mini_image, _, err  := image.Decode(mini_file)
-	//if err != nil {
-		//fmt.Print("Error @ img6")
-		//fmt.Println(err)
-	//}
-
-////crop the mini image(this needs to be resize or something really)
-	//cropped_mini := image.NewRGBA(image.Rect(0, 0, 10, 10))
-	//draw.Draw(cropped_mini, cropped_mini.Bounds(), mini_image, image.Point{0,0}, draw.Src)
-	//analyseColors(cropped_mini)
-	//return cropped_mini
-//}
 
 func averageColor(img image.Image) (color.RGBA) {
 	bounds := img.Bounds()
@@ -184,22 +150,20 @@ func averageColor(img image.Image) (color.RGBA) {
 
 }
 
-func cropToSquare(img *image.RGBA) (*image.RGBA) {
-	var cropped_img *image.RGBA
-	var side_length int
-	if img.Bounds().Max.X > img.Bounds().Max.Y {
-		side_length = img.Bounds().Max.Y
-	} else {
-		side_length = img.Bounds().Max.X
+func createTileCanvas(tile_positions map[image.Point]string, mosaic *image.RGBA) {
+	for point, path := range tile_positions {
+		tile_file, err := os.Open(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer tile_file.Close()
+		tile_image, _, err := image.Decode(tile_file)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		resized_tile := imaging.Resize(tile_image, 10, 10, imaging.Lanczos)
+		r := image.Rectangle{point, point.Add(resized_tile.Bounds().Size())}
+		draw.Draw(mosaic, r, resized_tile, image.Pt(0,0), draw.Src)
 	}
-
-	cropped_img = image.NewRGBA(image.Rect(0, 0, side_length, side_length))
-	draw.Draw(cropped_img, cropped_img.Bounds(), img, image.Point{0,0}, draw.Src)
-
-	return cropped_img
 }
-
-//func getMatchingImage(average_color color.RGBA, image_color_averages map[string]color.RGBA) (*image.RGBA) {
-
-	//return matching_image
-//}
