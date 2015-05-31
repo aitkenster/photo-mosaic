3b6c61c3b4f6b71dad36fe9fe8fc0549ec87ad5a
Nicola Aitken <nicola@geckoboard.com>
2015-05-31 16:06:34 +0100

    converts image source into an array of image links

---
+++ image_source/client.go
@@ -0,0 +1,39 @@
+package image_source
+
+import (
+	"net/http"
+	"fmt"
+	"encoding/json"
+)
+
+func GetFlickrRecentPhotos() []string {
+	resp, err := http.Get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	type WrappedImage struct {
+		Image string `json:"m"`
+	}
+
+	type ImageLinks struct {
+		Media WrappedImage `json:"media"`
+	}
+
+	var data struct {
+		Items []ImageLinks `json:"items"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	var imageList []string
+	for _, entry := range data.Items {
+		imageList = append(imageList, entry.Media.Image)
+	}
+	fmt.Println(imageList)
+	return imageList
+}
+
